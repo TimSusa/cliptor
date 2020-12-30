e2414cc14b3c8e8a31324bd7648b0d97db310c9a
TimSusa <timmsusa@googlemail.com>
2020-12-30 22:26:30 +0100

    feat: Introduce fading of clips, so there is no bad clicking

--- src/Clip.jsx
+++ src/Clip.jsx
@@ -10,17 +10,26 @@ export function Clip(props) {
   const dispatch = useDispatch()
   const { changeClipSrc } = actionsContent
   const { src, tracksId, clipId } = props
-  const [play, { stop, pause, isPlaying }] = useSound(src)
+  const [play, { stop, pause, isPlaying, sound }] = useSound(src, {
+    interrupt: false
+  })
   return (
     <React.Fragment>
-      <Typography>{src.substr(src.length - 25)}</Typography>
+      <Typography>{src.substr(src.length - 35)}</Typography>
+      <Typography>
+        {(sound && Array.isArray(sound._sounds) && sound._sounds[0]._seek) || 0}
+      </Typography>
       <Button
         style={{ marginLeft: 8, background: isPlaying ? 'green' : 'grey' }}
         draggable='false'
         onClick={() => {
           if (isPlaying) {
+            sound.fade(1, 0, 500)
+
             pause()
           } else {
+            sound.fade(0, 1, 500)
+
             play()
           }
         }}
@@ -47,7 +56,13 @@ export function Clip(props) {
         {isPlaying ? 'Pause' : 'Play' || 'None'}
       </Button>
       {isPlaying && (
-        <Button variant='outlined' onClick={stop}>
+        <Button
+          variant='outlined'
+          onClick={() => {
+            sound.fade(1, 0, 500)
+            stop()
+          }}
+        >
           Stop
         </Button>
       )}
