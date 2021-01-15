be52630378b0ffb9194bae42b7fb176c44ce2506
Daniel Vogel <herr.d.vogel@gmail.com>
2021-01-15 21:45:25 +0100

    suspend audioContext if no clip is playing

--- src/menu-app-bar/TimerClockLabel.js
+++ src/menu-app-bar/TimerClockLabel.js
@@ -1,11 +1,18 @@
 import React, { useContext, useState } from 'react'
 import { useRaf } from '../utils/useRaf'
 import context from '../global-state/context'
+import { useSelector } from 'react-redux'
 
 export function TimerClockLabel() {
   const { audioContext } = useContext(context)
+  const tracks = useSelector((state) => state.content.tracks)
   const [time, setTime] = useState('')
   useRaf(() => setTime(audioContext.currentTime))
+
+  const isPlaying = tracks.map(track => track.data.some(clip => clip.isPlaying)).some(Boolean)
+  if(!isPlaying) {
+    audioContext.suspend()
+  }
   return <div style={{ lineHeight: 3.5 }}>{msToTime(time * 1000)}</div>
 }
 
